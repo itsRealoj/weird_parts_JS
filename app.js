57a25f5d3762f56ebc4f7b2e26d8bf396afd4b69
Stephen Ochieng <stephenojwang1040@gmail.com>
2020-01-24 17:18:28 +0300

    Various approach to functions

---
+++ app.js
@@ -0,0 +1,15 @@
+var head = document.getElementById("hea");
+var btn = document.getElementById("my_btn");
+// btn.onclick(console.log('feeling blessed'))
+
+// const changeColor = () => {
+//     console.log("You are doing great");
+// }
+
+// function changeColor(){
+//     console.log("Woow this is awsome")
+// }
+
+(changeColor = (a,b) => {
+    console.log(a + b)
+})
