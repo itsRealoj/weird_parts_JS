edb6e1405e3c156467e92950c84bf838c66050b1
Stephen Ochieng <stephenojwang1040@gmail.com>
2020-01-25 09:56:30 +0300

    scope

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-var head = document.getElementById("hea");
-var btn = document.getElementById("my_btn");
-// btn.onclick(console.log('feeling blessed'))
+let head = document.getElementById("hea");
+let btn = document.getElementById("my_btn");
 
 // const changeColor = () => {
 //     console.log("You are doing great");
@@ -11,5 +10,29 @@ var btn = document.getElementById("my_btn");
 // }
 
 (changeColor = (a,b) => {
+
+    let person = {
+        name: "John Doe",
+        age: 17,
+        country: "Kenya",
+        education: [{kcpe: "A 82", kcse: "A 92",programming: "Second upper"}, "I am happy"]
+    }
+
+    let mood = document.getElementById("mood");
+    let date = document.getElementById("date");
+    let grade = document.getElementById("grade");
+
     console.log(a + b)
+
+    // for(let i = 0; i<3; i++){
+    //     console.log("Having agreat day")
+        console.log(person.education[1])
+    // }
+
+    person.education.forEach(element => {
+        console.log(element)
+    });
+
+    mood.innerHTML = person.education[0].kcpe;
+    alert(person.education[0].kcpe)
 })
---
+++ practice/app.js
@@ -0,0 +1,23 @@
+// ES6 IIFE
+
+// (() => {
+//     let time = new Date() ;
+//     console.log(time);
+// }) ();
+
+// Operator precedence
+
+// var sum = 5 + 5 * 3;
+// console.log(sum)
+
+var a;
+
+//goes out to the internet and looks for value
+
+a = 0;
+
+if(a){
+    console.log('We found a')
+} else {
+    console.log('not available')
+}
