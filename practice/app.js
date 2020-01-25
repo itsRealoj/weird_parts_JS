a4354e7bef1da72b6b24e32cb8a1ea8f13ba806c
Stephen Ochieng <stephenojwang1040@gmail.com>
2020-01-25 10:35:28 +0300

    scope

--- practice/app.js
+++ practice/app.js
@@ -10,14 +10,56 @@
 // var sum = 5 + 5 * 3;
 // console.log(sum)
 
-var a;
+
 
 //goes out to the internet and looks for value
 
-a = 0;
+// var a;
+// a = 0;
 
-if(a){
-    console.log('We found a')
-} else {
-    console.log('not available')
+// if(a){
+//     console.log('We found a')
+// } else {
+//     console.log('not available')
+// }
+
+// OBJECTS
+
+
+var person = new Object();
+
+person["firstName"] = "John";
+person["lastName"] = "Doe";
+
+var firstNameProperty = "firstName";
+
+console.log(person);
+console.log(person["firstName"])
+
+
+// OBJECTS SIMPLE FORM
+
+// var person = new Object();
+
+// person.firstName = "Jane"
+// person.lastName = "Doe"
+
+// console.log(person)
+
+// THE DOT
+
+var person = {
+    firstName: "John",
+    lastName: "Doe",
+    address: {
+        city: "new york",
+        street: "11 main St.",
+        state: "MN"
+    }
 }
+
+console.log(person)
+
+
+// OBJECT SHORTHAND
+
